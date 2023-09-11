225ebfffd1d07c3556f45e5218cc208863ae9909
Stefan <stefansandorovssb@gmail.com>
2023-09-11 23:05:09 +0200

    working on writing code into the console

---
+++ cpp-tour/basics/loops/loop.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+bool accept()
+{
+    int tries = 1;
+
+    while (tries < 4) {
+        cout << "Do you want to proceed (y or n)?\n";
+
+        char answer = 0;
+
+        cin >> answer;
+
+        switch (answer) {
+        case 'y':
+            return true;
+
+        case 'n':
+            return false;
+        
+        default:
+            cout << "Sorry, I don't understand that.\n";
+
+        }
+        
+        ++tries;
+    }
+
+    cout << "I'll take that for a no.\n";
+
+    return false;
+}
+
+int main()
+{
+    // This is not the correct way of executing the code, because
+    // I can't write anything, I just receive the console log
+    // accept();
+    accept();
+}
