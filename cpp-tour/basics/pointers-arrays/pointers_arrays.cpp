1a85ca5499e87d85705b87bc14834c4a1619581d
Stefan <stefansandorovssb@gmail.com>
2023-09-14 22:00:30 +0200

    pointers, arrays and loops

--- cpp-tour/basics/pointers-arrays/pointers_arrays.cpp
+++ cpp-tour/basics/pointers-arrays/pointers_arrays.cpp
@@ -21,10 +21,27 @@ void rangeForArray()
     for ( auto x : v ) {
         std::cout << x << "\n";
     }
+
+    for ( auto x : { 10, 21, 32, 43, 54, 65 } ) {
+        std::cout << x << "\n";
+    }
+}
+
+void increment()
+{
+    int v[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // here '&' stand for --> reference to
+    for ( auto& x : v ) {
+        ++x;
+
+        std::cout << x << "\n";
+    }
 }
 
 int main()
 {
     // copy_fct();
-    rangeForArray();
+    // rangeForArray();
+    increment();
 }
